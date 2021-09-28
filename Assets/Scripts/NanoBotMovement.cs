c6f1d281332333246626f769021b52c6028d717d
AlyfAiman <m.alyfaiman@gmail.com>
2021-09-29 02:53:36 +0800

    tried using navmesh but for some reason it works but not really
    the navmesh doesn't display on the scene

---
+++ Assets/Scripts/NanoBotMovement.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NanoBotMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform target;
+
+    public LayerMask whatIsGround, whatIsTarget;
+
+    public Animator animator;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBtwAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool targetInSightRange, targetInAttackRange;
+
+
+    private void Awake()
+    {
+        target = GameObject.Find("EnemyUnit1").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+
+
+    void Update()
+    {
+        //Check for sight and attack range
+        targetInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsTarget);
+        targetInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsTarget);
+
+        if (!targetInSightRange && !targetInAttackRange) Patroling();
+        if (targetInSightRange && !targetInAttackRange) ChaseTarget();
+        if (targetInAttackRange && targetInSightRange) AttackTarget();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+            //Walkpoint Reached
+            if(distanceToWalkPoint.magnitude < 1f)
+            {
+                walkPointSet = false;
+            }
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+
+    }
+
+    private void ChaseTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        //Make sure unit doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(target);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+            animator.SetBool("isAttacking", true);
+
+            ///
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBtwAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+        animator.SetBool("isAttacking", false);
+    }
+}
